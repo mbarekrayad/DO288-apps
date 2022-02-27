58c38f2cf6bc82e841257a5c363ad8cabebedb61
mbarekrayad <43930549+mbarekrayad@users.noreply.github.com>
2022-02-27 21:17:20 +0100

    Create mailer2.py

---
+++ s2i-scripts/mailer2.py
@@ -0,0 +1,11 @@
+    import mailer
+
+    message = mailer.Message()
+    message.From = "marocsosinfo@gmail.com"
+    message.To = "marocsosinfo@gmail.com"
+    message.Subject = "My Vacation"
+    message.Body = open("letter.txt", "rb").read()
+    message.attach("picture.jpg")
+
+    sender = mailer.Mailer('marocsosinfo@gmail.com')
+    sender.send(message)
