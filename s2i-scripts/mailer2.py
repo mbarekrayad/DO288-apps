c33b25004e19f9687eff831db05258ffdac3b501
mbarekrayad <43930549+mbarekrayad@users.noreply.github.com>
2022-02-27 21:42:50 +0100

    Update mailer2.py

--- s2i-scripts/mailer2.py
+++ s2i-scripts/mailer2.py
@@ -10,12 +10,11 @@ from mailer import Message
 import urllib
 
 msg1 = Message(From="translation@ginstrom.com",
-                  To=["marocsosinfo@gmail.com"],
+                  To="marocsosinfo@gmail.com",
                   charset="utf-8")
 msg1.Subject = "Vaccin"
-msg1.Html = """Hello""
+msg1.Html = """Hello, <b>日本語</b>"""
 
 mailer = Mailer('smtp01.odn.ne.jp')
 
 mailer.send(msg1)
-
